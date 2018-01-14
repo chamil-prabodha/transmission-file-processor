85233d351f111ee0199da5b9d7e14d0456dc9361
chamilp <chamil.prabodha@trycake.com>
2018-01-15 03:22:28 +0530

    fix bug

--- controllers/archive.js
+++ controllers/archive.js
@@ -9,7 +9,7 @@ var archive = function (filename) {
   var deferred = q.defer();
   var command = 'tar -zcvf '+
       global.appConfig.server.archiveDir + filename.replace(/(\s+)/g, '\\$1') + global.appConfig.server.archiveFormat + ' ' +
-      global.appConfig.server.downloadDir + filename;
+      global.appConfig.server.downloadDir + filename.replace(/(\s+)/g, '\\$1');
   var child = exec(command, function (error, stdout, stderr) {
     if(error) {
       deferred.reject(error);
